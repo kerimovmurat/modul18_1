6584ff9334d38d7cc88b90bd1dd36c86d5e0a2b9
Мурат <kerimovmurat@yandex.ru>
2024-12-26 10:52:00 +0300

    homework2

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from task2.views import index, index2
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('index/', index2.as_view())
 ]
---
+++ UrbanDjango/task2/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+def index(request):
+    return render(request, 'second_task/index.html')
+
+class index2(TemplateView):
+    template_name = 'second_task/index2.html'
+
