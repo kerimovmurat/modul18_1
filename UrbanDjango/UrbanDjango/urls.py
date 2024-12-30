09162ae38adc97d8d66cb9e681028be0c76bc546
Мурат <kerimovmurat@yandex.ru>
2024-12-30 23:44:09 +0300

    homework 4

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from task2.views import index, index2
-from task3.views import index, index2, index3
+# from task3.views import index, index2, index3
+from task4.views import index, index2, index3
 
 urlpatterns = [
     path('admin/', admin.site.urls),
---
+++ UrbanDjango/task4/views.py
@@ -0,0 +1,33 @@
+from django.views.generic import TemplateView
+
+# Create your views here.
+class index(TemplateView):
+    template_name = 'fourth_task/index.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        context['main'] = 'Главная'
+        context['vitamin'] = 'Витамины'
+        context['medicines'] = 'Лекарства'
+        return context
+
+class index2(TemplateView):
+    template_name = 'fourth_task/index2.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Витамины'
+        context['vit1'] = 'Витамины А'
+        context['vit2'] = 'Витамины В'
+        context['vit3'] = 'Витамины С'
+        context['buy'] = 'Купить'
+        context['back'] = 'Вернуться обратно'
+        return context
+
+class index3(TemplateView):
+    template_name = 'fourth_task/index3.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Лекарства'
+        context['message'] = 'Извините в данный момент препарат отсутствует'
+        context['back'] = 'Вернуться обратно'
+        return context
